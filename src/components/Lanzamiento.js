3e0eb253023fd51f31af088423cd2c69612466b4
Huahwl <acataf@uc.cl>
2019-10-20 04:51:35 -0300

    Todas las Vistas

---
+++ src/components/Lanzamiento.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, Button } from "react-native";
+import { StackNavigator } from "react-navigation";
+
+//import { principal } from "../styles/principal";
+//import { colors } from "../styles/colors";
+
+//import * as firebase from "firebase";
+
+export default class Lanzamiento extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+
+  }
+
+
+  componentDidMount() {
+
+  }
+
+  render() {
+
+      return (
+        <View>
+          <Text>Login</Text>
+        </View>
+
+      );
+
+  }
+}
